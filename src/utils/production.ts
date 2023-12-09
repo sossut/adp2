f316b3bf0e402c7650dfd5c532f4c9eed8dc0c65
sossut <mikki.alakari@elisanet.fi>
2023-12-09 17:04:21 +0200

    render ssl

--- src/utils/production.ts
+++ src/utils/production.ts
@@ -4,13 +4,11 @@ import { Request, Response, NextFunction } from 'express';
 
 const production = (app: any, port: string, httpsPort: string) => {
   app.enable('trust proxy');
-  const sslcert = fs.readFileSync('/etc/letsencrypt/live/shtsvr.mooo.com/fullchain.pem');
-  const sslkey = fs.readFileSync('/etc/letsencrypt/live/shtsvr.mooo.com/privkey.pem');
-  const ca = fs.readFileSync('/etc/letsencrypt/live/shtsvr.mooo.com/chain.pem')
+  // const sslkey = fs.readFileSync('/etc/pki/tls/private/ca.key');
+  const sslcert = fs.readFileSync('/etc/secrets/ca.crt.pem');
   const options = {
-    key: sslkey,
-    cert: sslcert,
-    ca : ca
+    // key: sslkey,
+    cert: sslcert
   };
   app.use((req: Request, res: Response, next: NextFunction) => {
     if (req.secure) {
@@ -22,7 +20,6 @@ const production = (app: any, port: string, httpsPort: string) => {
   });
   app.listen(port);
   https.createServer(options, app).listen(httpsPort);
-  //https.createServer(app).listen(httpsPort);
   console.log(`Listening: https://localhost:${httpsPort}`);
 };
 
